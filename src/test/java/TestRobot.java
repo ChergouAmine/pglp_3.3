b338c2d06482aec8d63d94e94f0e91ff23f7cbf4
Utilisateur <user@debian.arcahe.ovh>
2020-03-28 16:19:20 +0100

    constructeur test

---
+++ src/test/java/TestRobot.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class TestRobot {
+
+  @Test
+  public void testConstructeurStatique() {
+          RobotStatique r=new RobotStatique(5);
+          assert(r.position==5);        
+  }
+  
+  
+  @Test
+  public void testConstructeurDynamique() {
+          RobotDynamique r=new RobotDynamique(5);
+          assert(r.position==5);        
+  }
+  
+}
